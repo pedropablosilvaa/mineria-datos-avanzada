a54945a1bd9fe0de36e40bdf4f79dbfd2ff8c6ca
pedropablosilvaa <pedropablosilvaa@gmail.com>
2022-06-14 19:55:46 -0400

    WIP: update lab 5 SVM

--- lab-5_svm/lab-5_svm.R
+++ lab-5_svm/lab-5_svm.R
@@ -3,9 +3,11 @@
 # Alumno: Pedro Pablo Silva Antilef
 # Magister en Ingniería Informática - Universidad de Santiago de Chile
 
+# get path of current file
+library("rstudioapi") 
 library(e1071)
 library(RWeka)
-
+library(caTools)
 
 #--------------------------------------------------------------------
 #-------------------------- Carga de datos   ------------------------
@@ -55,13 +57,80 @@ print(ranking)
 #--------------------------------------------------------------------
 
 
+#------------------------- Dataset completo   -----------------------
+
+x <- subset(data, select = -type)
+y <- type
+
+# Kernel lineal considerando dataset completo
+set.seed(40)
+obj <- tune(svm, type~., data = data, kernel = "linear",ranges = list(cost = 2^(-3:6)), tunecontrol = tune.control(sampling = "cross", cross = 2 ))
+summary(obj)
+# Se plotea los errores con los costos
+plot(obj)
+summary(obj$best.model)
+
+# se genera la prediccion con el mejor modelo
+pred <- predict(obj$best.model, x)
+table(pred, type)
+
+# kernel radial
+obj <- tune(svm, type~., data = data, kernel = "radial", ranges = list(gamma = 2^(-4:8), cost = 2^(-3:6), tunecontrol = tune.control(sampling = "cross", cross = 2 )))
+summary(obj)
+plot(obj$gamma)
+
+pred <- predict(obj$best.model, x)
+table(pred, type)
+
+
+
+#------------------------- Dataset editado   -----------------------
+
+#seleccion de caracteristicas
+data_edited = data[,c('area','l_kernel', 'l_groove','type')]
+
+# kernel lineal
+obj <- tune(svm, type~., data = data_edited, kernel = "linear",ranges = list(cost = 2^(-3:6)), tunecontrol = tune.control(sampling = "cross", cross = 2 ))
+summary(obj)
+plot(obj)
+
+pred <- predict(obj$best.model, x)
+table(pred, type)
+
+
+# kernel radial
+obj <- tune(svm, type~., data = data_edited, kernel = "radial", ranges = list(gamma = 2^(-4:8), cost = 2^(-3:6), tunecontrol = tune.control(sampling = "cross", cross = 2 )))
+summary(obj)
+pred <- predict(obj$best.model, x)
+table(pred, type)
+
+
+#set.seed(40)
+#spliting dataset
+#split = sample.split(data$type, SplitRatio = 0.65)
+#training_set = subset(data, split == TRUE)
+#test_set = subset(data, split == FALSE)
+
+#svm on trianing set
+#classifier = svm(formula = type ~ .,
+#                 data = training_set,
+#                 type = 'C-classification',
+#                 kernel = 'linear')
+
+#y_pred = predict(classifier, newdata = test_set[-8])
+#cm = table(test_set[, 8], y_pred)
+#cm
+
+
+
+
+#s
+
+
+
+
+
 attach(data)
-
-formula=type ~ .
-model <- svm(formula, data = data)
-print(model)
-summary(model)
-
 
 x <- subset(data, select = -type)
 y <- type
@@ -75,27 +144,5 @@ pred <- predict(model, x, decision.values = TRUE)
 attr(pred, "decision.values")[1:4,]
 
 # visualize (classes by color, SV by crosses):
-plot(cmdscale(dist(data[,-7])), col = as.integer(data[,7]), pch = c("o","+")[1:150 %in% model$index + 1])
+plot(cmdscale(dist(data[,-8])), col = as.integer(data[,8]), pch = c("o","+")[1:150 %in% model$index + 1])
 
-
-obj <- tune(svm, type~., data = data, kernel = "linear",ranges = list(cost = 2^(-1:4)), tunecontrol = tune.control(sampling = "cross", cross = 2 ))
-summary(obj)
-
-plot(obj)
-summary(obj$best.model)
-
-
-pred <- predict(obj$best.model, x)
-table(pred, type)
-
-
-obj <- tune(svm, type~., data = data, kernel = "radial", ranges = list(gamma = 2^(-2:4), cost = 2^(-1:4), tunecontrol = tune.control(sampling = "cross", cross = 2 )))
-summary(obj)
-
-
-pred <- predict(obj$best.model, x)
-table(pred, type)
-
-obj <- tune(svm, type~., data = data, kernel = "radial", ranges = list(gamma = 2^(-7:12), cost = 2^(-7:14) , tunecontrol = tune.control(sampling = "cross", cross = 2)))
-pred <- predict(obj$best.model, x)
-table(pred, type)
